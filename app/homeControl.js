e10ad3c2aac223acf0c4161db4dbd0da86bb1ecc
francisluz <francisluz@gmail.com>
2014-06-05 01:39:38 -0300

    Novo Layout
    Pagina home e melhorias no layout

--- app/homeControl.js
+++ app/homeControl.js
@@ -8,11 +8,12 @@ var myApp = angular.module('myApp', ['ngResource'])
 
 myApp.controller('HomeCtrl', function ($scope, $resource, $http) {
   	
-  	$scope.nome = '+Francis Luz';
+  	$scope.nome = 'Francis Luz';
+  	$scope.descricao = 'Technologic live';
 
   	$scope.posts = [];
 
-	$http.get('https://www.googleapis.com/plus/v1/people/107152405442983389032/activities/public?maxResults=10&key=AIzaSyAk3R-lwNSuZrojcebvKKNHRxBd5RzF6DQ').success(function(data) {
+	$http.get('https://www.googleapis.com/plus/v1/people/107152405442983389032/activities/public?maxResults=9&key=AIzaSyAk3R-lwNSuZrojcebvKKNHRxBd5RzF6DQ').success(function(data) {
     	angular.forEach(data.items, function(post, index) {
           if (post.verb == 'post') 
            $scope.posts.push(post.object);
